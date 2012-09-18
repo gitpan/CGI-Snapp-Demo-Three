6fbaa02101fbdae0da160d8ff3d0741995de0515
Ron Savage <ron@savage.net.au>
2012-09-18 03:10:04 +0000

    Import of RSAVAGE/CGI-Snapp-Demo-Three-1.01 from CPAN.
    gitpan-cpan-distribution: CGI-Snapp-Demo-Three
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         RSAVAGE/CGI-Snapp-Demo-Three-1.01.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- lib/CGI/Snapp/Demo/Three.pm
+++ lib/CGI/Snapp/Demo/Three.pm
@@ -4,9 +4,7 @@ use parent 'CGI::Snapp';
 use strict;
 use warnings;
 
-use CGI::Snapp::Plugin::Forward; # For forward().
-
-our $VERSION = '1.00';
+our $VERSION = '1.01';
 
 # ------------------------------------------------
 
@@ -181,7 +179,7 @@ sub third_rm_prerun
 
 =head1 NAME
 
-CGI::Snapp::Demo::Three - A template-free demo of CGI::Snapp using CGI::Snapp::Plugin::Forward
+CGI::Snapp::Demo::Three - A template-free demo of CGI::Snapp using the forward() method
 
 =head1 Synopsis
 
@@ -225,7 +223,7 @@ These commands are copied from comments within httpd/cgi-bin/cgi.snapp.three.psg
 
 =head1 Description
 
-Shows how to use a plugin within L<CGI::Snapp>, in this case L<CGI::Snapp::Plugin::Forward>.
+Shows how to use the forward() method to interrupt processing of a run mode.
 
 The output reports which methods were and were not entered per run mode.
 
@@ -314,15 +312,11 @@ The following are all part of this set of distros:
 
 L<CGI::Snapp> - A almost back-compat fork of CGI::Application
 
-L<CGI::Snapp::Plugin::Forward> - A plugin for CGI::Snapp to switch cleanly to another run mode within the same app
-
-L<CGI::Snapp::Plugin::Redirect> - A plugin for CGI::Snapp to simplify using HTTP redirects
-
 L<CGI::Snapp::Demo::One> - A template-free demo of CGI::Snapp using just 1 run mode
 
 L<CGI::Snapp::Demo::Two> - A template-free demo of CGI::Snapp using N run modes
 
-L<CGI::Snapp::Demo::Three> - A template-free demo of CGI::Snapp using CGI::Snapp::Plugin::Forward
+L<CGI::Snapp::Demo::Three> - A template-free demo of CGI::Snapp using the forward() method
 
 L<CGI::Snapp::Demo::Four> - A template-free demo of CGI::Snapp using Log::Handler::Plugin::DBI
 
